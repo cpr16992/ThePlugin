bef1120d9dd66d0b4028c49edbb4631c31bc8662
carlos <carlos@flemming.cnb.csic.es>
2013-08-26 16:30:12 +0200

    Sustituidos los checkboxes por radiobuttons en los dos casos en los que necesitamos selección exclusiva. El método "illegalQuery" no lo he eliminado ya que quizás sea necesario más tarde.

--- src/Atlas_Selector.java
+++ src/Atlas_Selector.java
@@ -30,73 +30,62 @@ public class Atlas_Selector extends PlugInFrame{
 		Vector<Checkbox> vect = gd1.getCheckboxes();
 		boolean uno = vect.get(0).getState();
 		boolean dos = vect.get(1).getState();
-		boolean works = false;
-		while (!works)
+		GenericDialog gd2 = new GenericDialog("Checkout selection");
+		String options[] = {"Anatomy", "Gene"};
+		gd2.addMessage("Please select the query type:");
+		gd2.addRadioButtonGroup(null, options, 2, 1, "Anatomy");
+		gd2.showDialog();
+		if (gd2.wasCanceled()) {
+			IJ.error("PlugIn canceled!");
+			return;
+		}
+		String result1 = gd2.getNextRadioButton();
+		if (result1.equals(null))
 		{
-			GenericDialog gd2 = new GenericDialog("Checkout selection");
-			String options[] = {"Anatomy", "Gene"};
-			gd2.addMessage("Please select the query type:");
-			gd2.addCheckboxGroup(2, 1, options, defaultvalues);
-			gd2.showDialog();
-			if (gd2.wasCanceled()) {
-				IJ.error("PlugIn canceled!");
-				return;
+			illegalQuery();
+		}
+		else if (result1.equals("Anatomy"))
+		{
+			if (uno)
+			{
+				if (dos)
+				{
+
+				}
+				else
+				{
+
+				}
 			}
-			Vector<Checkbox> vectr = gd2.getCheckboxes();
-			boolean tres = vectr.get(0).getState();
-			boolean cuatro = vectr.get(1).getState();
-			if (cuatro && tres)
+			else if (dos)
+			{
+
+			}
+
+		}
+		else if (result1.equals("Gene"))
+		{
+			GenericDialog gd4 = new GenericDialog("Gene Query");
+			String queries[] = {"Individual expression", "Coexpression"};
+			gd4.addMessage("Please select a query:");
+			gd4.addRadioButtonGroup(null, queries, 2, 1, "Individual expression");
+			gd4.showDialog();
+			String result2 = gd4.getNextRadioButton();
+			if (result2.equals(null))
 			{
 				illegalQuery();
 			}
-			else if (tres)
+			else if (result2.equals("Individual expression"))
 			{
-				works = true;
-				if (uno)
-				{
-					if (dos)
-					{
-
-					}
-					else
-					{
-
-					}
-				}
-				else if (dos)
-				{
-
-				}
-
 			}
-			else if (cuatro)
+			else if (result2.equals("Coexpression"))
 			{
-				GenericDialog gd4 = new GenericDialog("Gene Query");
-				String queries[] = {"Individual expression", "Coexpression"};
-				gd4.addMessage("Please select a query:");
-				gd4.addCheckboxGroup(2, 1, queries, defaultvalues);
-				gd4.showDialog();
-				Vector<Checkbox> vector = gd4.getCheckboxes();
-				boolean cinco = vector.get(0).getState();
-				boolean seis = vector.get(1).getState();
-				if (cinco && seis)
-				{
-					illegalQuery();
-				}
-				else if (cinco)
-				{
-					works = true;
-				}
-				else if (seis)
-				{
-					works = true;
-				}
-
 			}
+
 		}
-
-
 	}
+
+
 	private void illegalQuery() {
 		GenericDialog gd = new GenericDialog("Error");
 		gd.addMessage("Error. Illegal query.");
