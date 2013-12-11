17b12267628facdbba201409f39be027e234867a
tfg-biig <tfg-biig@lim2.lim.local>
2013-12-11 17:32:04 +0100

    El parseador del atlas MRM funciona.

--- src/MRMparser.java
+++ src/MRMparser.java
@@ -1,23 +1,14 @@
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class MRMparser extends ArrayList<Structure>
 {
-	/** Reads tags from a .txt file and stores them in a Hashtable
-	 * @param fileName the path and name of the file containing tags
-	 */
 	private static final long serialVersionUID = 1L;
 	private static ArrayList<Structure> MRMTags = new ArrayList<Structure>();
-	private int num;
 	public MRMparser(String fileName)
 	{
 		/*
@@ -29,16 +20,14 @@ public class MRMparser extends ArrayList<Structure>
 		 */
 		try
 		{
-			String test = readFile(fileName);
-			String[] arr = test.split("\t");
-			System.out.println(arr[0]);
-			for(int a = 0; a <= arr.length; a = a + 4)
-			{
+			ArrayList<String> test = readFile(fileName);
+			for (String s : test) {
+				String [] arr = s.split("\t");				
 				Structure str = new Structure();
-				str.setId(arr[a]);
-				str.setColorHexTriplet(arr[a+1]);	
-				str.setAcronym(arr[a+2]);
-				str.setName(arr[a+3]);
+				str.setId(arr[0]);
+				str.setColorHexTriplet(arr[1]);
+				str.setAcronym(arr[2]);
+				str.setName(arr[3]);
 				MRMTags.add(str);
 			}
 		}
@@ -52,28 +41,27 @@ public class MRMparser extends ArrayList<Structure>
 	 */
 	 public void printTags()
 	{
-		for (int i = 0; i <= MRMTags.size(); i++)
+		for (int i = 0; i < MRMTags.size(); i++)
 		{
 			System.out.println(MRMTags.get(i).getName());
 		}
 	}
 
-	 private String readFile( String file ) throws IOException {
-		    BufferedReader reader = new BufferedReader( new FileReader (file));
-		    String         line = null;
-		    StringBuilder  stringBuilder = new StringBuilder();
-		    String         ls = System.getProperty("line.separator");
+	private ArrayList<String> readFile(String file) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line = null;
+		ArrayList<String> lines = new ArrayList<String>();
 
-		    while( ( line = reader.readLine() ) != null ) {
-		        stringBuilder.append( line );
-		        stringBuilder.append( ls );
-		    }
-
-		    return stringBuilder.toString();
+		while ((line = reader.readLine()) != null) {
+			lines.add(line);
 		}
+		reader.close();
+
+		return lines;
+	}
 	 
 	public static void main(String[] args){
 		 MRMparser etiquetas = new MRMparser("C:\\Documents and Settings\\tfg-biig\\Desktop\\MRM.txt");
-		 //etiquetas.printTags();
+		 etiquetas.printTags();
 	}
 }
